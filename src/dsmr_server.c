c3c3c91551abd10048e1a3f438d18da7d5b74a97
Vincent Smeets <Vincent.VSmeets@GMail.com>
2023-11-29 20:21:38 +0100

    src/dsmr_server.c

---
+++ src/dsmr_server.c
@@ -0,0 +1,24 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2023 Vincent Smeets
+ */
+
+/* includes */
+#include <stdlib.h>
+
+/* defines */
+
+/* external declarations */
+
+/* typedefs */
+
+/* global variable declarations */
+
+/* static function */
+
+int
+main (int argc, char *argv[])
+{
+  return EXIT_SUCCESS;
+}
